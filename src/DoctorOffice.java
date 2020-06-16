b86caf4a6658f2180e2f47606eb7f8de6992a312
amde11 <amdeasrat@gmail.com>
2020-06-15 16:05:39 -0800

    Doctors office Assigment

---
+++ src/DoctorOffice.java
@@ -0,0 +1,25 @@
+public class DoctorOffice {
+
+        public static void main (String[] args) {
+            String patientName="Stacey Manson";
+            boolean hasInsurance=false;
+            double temp=102.2;
+            int numVisits=5;
+            float copayAmount=10000.50f;
+            String nextAppoDate="06/16/20";
+            String contPhoneNum="2058769876";
+
+            System.out.println("Patient Details:");
+            System.out.println("Name: "+patientName);
+            System.out.println("Patient has insurance: "+hasInsurance);
+            System.out.println("Body temprature: " + temp);
+            System.out.println("Number of Vist: " + numVisits);
+            System.out.println("pataint copy Amount: " + copayAmount);
+            System.out.println("Number of Vist: " + nextAppoDate);
+            System.out.println("Phone number: " + contPhoneNum);
+
+
+
+
+        }
+}
